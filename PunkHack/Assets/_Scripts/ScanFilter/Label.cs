f37d9ead3c68ff34a0296dbe33fa36a8dfccad2a
NabilCherigui <mrtopgamer@hotmail.com>
2017-08-16 01:08:36 +0200

    Day 1 - Version 0.1

---
+++ PunkHack/Assets/_Scripts/ScanFilter/Label.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Label : MonoBehaviour
+{
+    [SerializeField] private Scan _scan;
+    [SerializeField] private string _device;
+    [SerializeField] private string _activity;
+    [SerializeField] private string _sentiment;
+    [SerializeField] private string _issue;
+    [SerializeField] private string _faction;
+
+    private void Start()
+    {
+        _device = _scan.Device[Random.Range(0,2)];
+        _activity = _scan.Activity[Random.Range(0,2)];
+        _sentiment = _scan.Sentiment[Random.Range(0,2)];
+        _issue = _scan.Issue[Random.Range(0,2)];
+        _faction = _scan.Faction[Random.Range(0,2)];
+        
+    }
+}
---
+++ PunkHack/Assets/_Scripts/ScanFilter/Scan.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scan : MonoBehaviour
+{
+	[SerializeField] private GameObject[] _people;
+	[SerializeField] private string[] _device;
+	[SerializeField] private string[] _activity;
+	[SerializeField] private string[] _sentiment;
+	[SerializeField] private string[] _issue;
+	[SerializeField] private string[] _faction;
+
+	public string[] Device { get {return _device;}}
+	public string[] Activity { get {return _activity;}}
+	public string[] Sentiment { get {return _sentiment;}}
+	public string[] Issue { get {return _issue;}}
+	public string[] Faction { get {return _faction;}}
+	
+}
