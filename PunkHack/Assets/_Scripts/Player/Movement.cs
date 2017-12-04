29f3eddfd40c18186d5c4424e57c5bf589e82c41
NabilCherigui <mrtopgamer@hotmail.com>
2017-12-04 20:31:21 +0100

    Update

---
+++ PunkHack/Assets/_Scripts/Player/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+	[SerializeField] private Vector2 _target;
+	[SerializeField] private float _speed;
+	[SerializeField] private bool _go;
+
+	private void Update()
+	{
+		if (_go)
+		{
+			transform.position = Vector2.MoveTowards(transform.position, _target, _speed);
+		}
+	}
+}
